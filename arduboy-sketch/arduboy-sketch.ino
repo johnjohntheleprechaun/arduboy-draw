60dcae8605d2145b895c5b2a955265dd76e8ed72
John Jorgensen <john@jorgensenfamily.us>
2024-04-12 02:23:04 -0700

    create basic arduboy sketch

---
+++ arduboy-sketch/arduboy-sketch.ino
@@ -0,0 +1,18 @@
+#include <Arduboy2.h>
+Arduboy2 arduboy;
+
+byte frameBuffer[1024];
+
+bool getPixelState(int y, int x) {
+    return frameBuffer[y*128+x];
+}
+
+void setup() {
+    arduboy.begin();
+    arduboy.clear();
+    arduboy.setFrameRate(30);
+    arduboy.allPixelsOn(true);
+}
+
+void loop() {
+}
